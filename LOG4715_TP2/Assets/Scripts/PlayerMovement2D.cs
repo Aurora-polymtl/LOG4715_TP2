991603d119343861ee328dbd099455129b382d5c
phhad <philippe.hadjis@polymtl.ca>
2025-10-25 14:41:58 -0400

    Formulation Gym2 et Gym3

--- LOG4715_TP2/Assets/Scripts/PlayerMovement2D.cs
+++ LOG4715_TP2/Assets/Scripts/PlayerMovement2D.cs
@@ -6,14 +6,17 @@ public class PlayerMove2D : MonoBehaviour
     [SerializeField] private float m_MaxSpeed = 10f;
     [SerializeField] private float m_JumpForce = 10f;                  // Amount of force added when the player jumps.
     [SerializeField] private int midair_Jumps = 1;
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private LayerMask wallLayer;
     private float currentJumpForce;
     private Rigidbody2D m_Rigidbody2D;
     private Animator m_animate;
-    private bool grounded;
+    private BoxCollider2D m_Collider;
     private void Awake()
     {
         m_Rigidbody2D = GetComponent<Rigidbody2D>();
         m_animate = GetComponent<Animator>();
+        m_Collider = GetComponent<BoxCollider2D>();
     }
     // Update is called once per frame
     private void Update()
@@ -30,31 +33,29 @@ public class PlayerMove2D : MonoBehaviour
             transform.localScale = new Vector3(-1, 1, 1);
         }
 
-        if (Input.GetKey(KeyCode.Space) && grounded)
+        if (Input.GetKey(KeyCode.Space) && isGrounded())
         {
             Jump();
         }
         m_animate.SetBool("run", horizontalInput != 0);
-        m_animate.SetBool("grounded", grounded);
+        m_animate.SetBool("grounded", isGrounded());
     }
 
     private void Jump()
     {
         m_Rigidbody2D.linearVelocity = new Vector2(m_Rigidbody2D.linearVelocity.x, m_JumpForce);
         m_animate.SetTrigger("jump");
-        grounded = false;
-    }
-
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if(collision.gameObject.tag == "Ground")
-        {
-            grounded=true;
-        }
     }
 
     private bool isGrounded()
     {
-        return false;
+        RaycastHit2D raycastHit = Physics2D.BoxCast(m_Collider.bounds.center, m_Collider.bounds.size, 0, Vector2.down, 0.1f, groundLayer);
+        return raycastHit.collider != null;
+    }
+
+    private bool isHittingWall()
+    {
+        RaycastHit2D raycastHit = Physics2D.BoxCast(m_Collider.bounds.center, m_Collider.bounds.size, 0, new Vector2(transform.localScale.x, 0), 0.1f, wallLayer);
+        return raycastHit.collider != null;
     }
 }
