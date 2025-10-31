2c3775b990b084a0bef119c383627bcf1ace8c28
phhad <philippe.hadjis@polymtl.ca>
2025-10-30 22:16:40 -0400

    Missing HealthBar
    Source: https://www.youtube.com/watch?v=yxzg8jswZ8A&t=463s

--- LOG4715_TP2/Assets/Scripts/Health.cs
+++ LOG4715_TP2/Assets/Scripts/Health.cs
@@ -2,22 +2,28 @@ using UnityEngine;
 
 public class Health : MonoBehaviour
 {
-    [SerializeField] private float health;
-    private float currentHealth;
+    [SerializeField] private float healthStart;
+    public float currentHealth { get; private set; }
     private void Awake()
     {
-        currentHealth = health;
+        currentHealth = healthStart;
     }
 
     public void TakingDamage(float _damage)
     {
-        currentHealth = Mathf.Clamp(currentHealth, 0, health);
+        currentHealth = Mathf.Clamp(currentHealth - _damage, 0, healthStart);
 
         if (currentHealth > 0) {
-
         } else
         {
 
+        }
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            TakingDamage(1);
         }
     }
 
