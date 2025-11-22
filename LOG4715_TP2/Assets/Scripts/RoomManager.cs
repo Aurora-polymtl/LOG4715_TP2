a66ad4466b7f2c691c34610133e499147cc2a83b
Adam Bouhadid <122629879+AdamBhd@users.noreply.github.com>
2025-11-22 12:42:29 -0500

    Camera par room

---
+++ LOG4715_TP2/Assets/Scripts/RoomManager.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class RoomManager : MonoBehaviour
+{
+    public GameObject VirtualCam;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player") && !collision.isTrigger)
+        {
+            VirtualCam.SetActive(true);
+        }
+
+
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player") && !collision.isTrigger)
+        {
+            VirtualCam.SetActive(false);
+        }
+    }
+}
