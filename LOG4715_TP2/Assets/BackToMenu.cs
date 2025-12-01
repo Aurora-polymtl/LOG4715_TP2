da91c671a176241b4e88e23b08459110debae57d
phhad <philippe.hadjis@polymtl.ca>
2025-12-01 17:39:20 -0500

    Son étoile

--- LOG4715_TP2/Assets/BackToMenu.cs
+++ LOG4715_TP2/Assets/BackToMenu.cs
@@ -6,11 +6,14 @@ public class BackToMenu : MonoBehaviour
     [SerializeField]
     [Tooltip("Name of the menu scene to load when the player touches this trigger")]
     private string menuSceneName = "MenuScene";
+    [SerializeField] private AudioClip victorySound;
+
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         // only react to the player touching the trigger
         if (!other.CompareTag("Player")) return;
+        SoundManager.instance.PlaySound(victorySound);
 
         // optional: ensure this collider is a trigger - usually set in the editor
         if (!GetComponent<Collider2D>())
